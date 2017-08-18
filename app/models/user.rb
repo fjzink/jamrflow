b3194383c5062ffcafc9904ccc58cbba8ebe326b
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-08-18 11:52:02 -0700

    Add validations to all models and methods 'points' + 'time_since_creation' to votables

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   has_many :comments, foreign_key: :commenter_id
   has_many :votes, foreign_key: :voter_id
 
+  validates :username, :email, presence: true, uniqueness: true
+
+
   def password
     @password ||= BCrypt::Password.new(hashed_password)
   end
