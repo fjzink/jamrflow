e74182f660f23743a27c5c229b220d676e64547c
Rio <email": null>
2017-08-18 11:06:35 -0700

    Fix logout route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ post '/login' do
   user = User.authenticate(params[:username], params[:password])
   if user
     login(user)
+    redirect '/questions'
   else
     @errors = user.errors.full_messages
+    redirect :'/login'
   end
 end
 
