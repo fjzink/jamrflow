72fbaa35ed26d3fef1375008a70a76ea54cc10a2
Michael Glaser <email": null>
2017-08-18 15:49:15 -0700

    Adds updates from the development branch.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,7 @@
 #show all comments associated with the given answer.
 get '/answers/:id/comments' do
-  @answer = Answer.find(1)
-  comments = Comment.all
-  @users = User.all
-  @commenter = ''
-  @comments = @comments.select { |comment| comment.commentable_type == "Answer" }
-  @comments = @comments.select { |comment| comment.commentable_id == params[:id] }
+  @answer = Answer.find(params[:id])
+  @comments = 
 
 
   erb :'/partials/_comments'
