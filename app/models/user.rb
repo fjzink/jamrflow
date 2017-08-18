6902b8712607865166f9f7dd92738f4480bc6921
Rio <email": null>
2017-08-17 18:15:55 -0700

    Implement session routes, views, and helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ActiveRecord::Base
   has_many :answers, foreign_key: :answerer_id
   has_many :comments, foreign_key: :commenter_id
   has_many :votes, foreign_key: :voter_id
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def self.authenticate(inputted_username, inputted_password)
+    user = self.find_by(username: inputted_username)
+    if user
+      if user.password == inputted_password
+        user
+      end
+    else
+      nil
+    end
+  end
 end
