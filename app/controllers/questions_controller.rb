cd75831236da8bc8e4f14c51b8eefa38356ee920
Rio <email": null>
2017-08-18 14:28:01 -0700

    Add voting to question page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,28 +7,28 @@ post '/questions/:id/upvote' do
   # content_type :json
   question = Question.find(params[:id])
   question.votes.create(value: 1)
-  redirect '/questions/:id/answers'
+  redirect "/questions/#{question.id}/answers"
 end
 
 post '/questions/:id/downvote' do
   # content_type :json
   question = Question.find(params[:id])
   question.votes.create(value: -1)
-  redirect '/questions/:id/answers'
+  redirect "/questions/#{question.id}/answers"
 end
 
-post '/questions/:id/comment' do
+post '/questions/:id/comments' do
   # content_type :json
   question = Question.find(params[:id])
-  redirect '/questions/:id/comments'
+  redirect "/questions/#{question.id}/comments"
 end
 
-get 'questions/:id/answers' do
+get '/questions/:id/answers' do
   @question = Question.find(params[:id])
   @answers = @question.answers
-  erb :'questions/show'
+  erb :'/questions/show'
 end
 
-get 'questions/:id/comments' do
-  erb :'questions/comments'
+get '/questions/:id/comments' do
+  erb :'/questions/comments'
 end
