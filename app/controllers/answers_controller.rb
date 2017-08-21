f516a6c3a5c51ba9356607be06d510530a7f81ee
Rio <rnose512@gmail.com>
2017-08-20 17:12:34 -0700

    Make comments work

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,6 +12,17 @@ post '/answers/:id' do
 
   if logged_in?
     Comment.Create(content: params[:comment], commentable_type: Answer, commenter_id: session[:user_id], commentable_id: answer.id)
+    redirect "/answers/#{answer.id}/comments"
+  else
+    redirect '/login'
+  end
+end
+
+post '/answers/:id/comments' do
+  if logged_in?
+    answer = Answer.find(params[:id])
+    Comment.create(content: params[:comment], commentable_type: Answer, commentable_id: params[:id], commenter_id: session[:user_id])
+    redirect "/answers/#{answer.id}/comments"
   else
     redirect '/login'
   end
